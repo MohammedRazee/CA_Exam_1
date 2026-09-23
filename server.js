8f17d7b30c180fe2a81c7ef78e8bbb5385a90169
razee <razeeismail15@gmail.com>
2026-09-23 19:42:39 +0530

    first commit

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
 // Write your code here
+import http from 'http';
+import fs from 'fs';
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('<h1>Home Page</h1>');
+  } else if (req.url === '/about') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('<h1>About Page</h1>');
+  }
+});
+
+server.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
